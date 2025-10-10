9b726fc2b1cb28c34c20edc176bc3ae49de4c01b
badii bayoudh <badii.bayoudh@gmail.com>
2025-10-10 11:50:44 +0200

    use observers

--- MyDatingApp/API/Program.cs
+++ MyDatingApp/API/Program.cs
@@ -13,19 +13,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>{
 });
 //builder.Services.AddOpenApi();
 
+builder.Services.AddCors();
 var app = builder.Build();
 
-/*
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseCors(policy => 
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("http://localhost:4200", "https://localhost:4200"));   
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-*/
 app.MapControllers();
 
 app.Run();
--- MyDatingApp/client/src/app/app.ts
+++ MyDatingApp/client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  private http = inject(HttpClient)
   protected readonly title = signal('client');
+  protected members = signal<any>([]);
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (Response) => this.members.set(Response),
+      error: (error) => console.error(error),
+      complete: () => console.log('Request complete')
+    })
+  }
+
+  //constructor(private http: HttpClient){}
 }
